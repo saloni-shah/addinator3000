a44994fa58c5cd5a5bcb9755e30d8f9a4be1c5e8
Saloni Shah <saloni.shah281@gmail.com>
2017-04-18 15:01:06 -0700

    commit after watchify

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const add = require('./addinator.js');
 
 class ViewManager{
 	connectEventHandlers(){
-
+		document.getElementById('form-numbers').addEventListener('submit',this.onSubmit);
 	}
 
 	onSubmit(event){
-
+		event.preventDefault();
+		let num1 = document.getElementById('input-num1').value;
+		let num2 = document.getElementById('input-num2').value;
+		num1 = parseInt(num1,10);
+		num2 = parseInt(num2,5);
+		const sum = add(num1,num2);
+		alert(sum);
+		console.log(sum);
 	}
 }
 
--- bundle.js
+++ bundle.js
@@ -5,5 +5,25 @@ const add = function(num1,num2){
 module.exports = add;
 },{}],2:[function(require,module,exports){
 const add = require('./addinator.js');
-console.log(add(5,6));
+//console.log(add(5,6));
+
+class ViewManager{
+	connectEventHandlers(){
+		document.getElementById('form-numbers').addEventListener('submit',this.onSubmit);
+	}
+
+	onSubmit(event){
+		event.preventDefault();
+		let num1 = document.getElementById('input-num1').value;
+		let num2 = document.getElementById('input-num2').value;
+		num1 = parseInt(num1,10);
+		num2 = parseInt(num2,5);
+		const sum = add(num1,num2);
+		alert(sum);
+		console.log(sum);
+	}
+}
+
+const viewManager = new ViewManager();
+viewManager.connectEventHandlers();
 },{"./addinator.js":1}]},{},[2]);
