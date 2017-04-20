da5636ac4a32e306c435e729e69dbe0be93c14ab
Saloni Shah <saloni.shah281@gmail.com>
2017-04-20 11:22:20 -0700

    add browserify dependency and port dependency

--- client/app.js
+++ client/app.js
@@ -3,18 +3,20 @@ const add = require('./addinator.js');
 
 class ViewManager{
 	connectEventHandlers(){
-		document.getElementById('form-numbers').addEventListener('submit',this.onSubmit);
+		document.getElementById('form-numbers').addEventListener('submit',this.onSubmit.bind(this));
 	}
 
 	onSubmit(event){
 		event.preventDefault();
 		let num1 = document.getElementById('input-num1').value;
 		let num2 = document.getElementById('input-num2').value;
-		num1 = parseInt(num1,10);
-		num2 = parseInt(num2,5);
+		num1 = parseInt(num1);
+		num2 = parseInt(num2);
 		const sum = add(num1,num2);
-		alert(sum);
-		console.log(sum);
+		this.renderSum(sum);
+	}
+	renderSum(sum){
+		document.querySelector('.sum').textContent = sum;
 	}
 }
 
